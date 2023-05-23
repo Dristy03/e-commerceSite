d7aff7a0730bf74dd3f22d1ff47a1cf67dba89a8
Dristy03 <ananna49@student.sust.edu>
2023-05-23 11:42:53 +0600

    cart ui added

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -10,31 +10,35 @@ export default function Cart() {
         <div className={styles.title}>Shopping Bag</div>
 
         <div className={styles.item}>
-          <div className={styles.buttons}>
-            <span className={styles.deletebtn}></span>
-            <span className={styles.likebtn}></span>
-          </div>
-
           <div className={styles.image}>
-            <img src="https://i.postimg.cc/76X9ZV8m/Screenshot_from_2022-06-03_18-45-12.png" alt="" height={200} width={200}/>
+            <img
+              src="https://i.postimg.cc/76X9ZV8m/Screenshot_from_2022-06-03_18-45-12.png"
+              alt=""
+              height={100}
+              width={100}
+            />
           </div>
 
-          <div className={styles.description}>
-            <span>Hoodie Jacket</span>
-        
-          </div>
+          <h1 className={styles.description}>Hoodie Jacket</h1>
 
           <div className={styles.quantity}>
             <button className={styles.plusbtn} type="button" name="button">
-              <img src="plus.svg" alt="" />
+              -
             </button>
             <input type="text" name="name" value="1" />
             <button className={styles.minusbtn} type="button" name="button">
-              <img src="minus.svg" alt="" />
+              +
             </button>
           </div>
 
-          <div className={styles.totalprice}>$549</div>
+          <div className={styles.totalprice}>Tk 3500</div>
+
+          <span className={styles.deletebtn}> Remove </span>
+        </div>
+
+        <div className={styles.bottom}>
+          <h3>Grand Total: <span>TK 3500 </span></h3>
+          <button className={styles.ck}>CHECKOUT</button>
         </div>
       </div>
     </>
